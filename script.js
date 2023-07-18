1211c4d1dbbda4efc0f255fa2ed3dc1b5690e55f
David Pallarés <serallapdivad@gmail.com>
2023-07-18 10:37:46 +0100

    Implementar validación en el formulario.

--- script.js
+++ script.js
@@ -31,14 +31,42 @@ document.addEventListener("DOMContentLoaded", function () {
         // when we scroll this event starts running
         window.addEventListener("scroll", function () {
             // the percertaje of the scroll is 10 or higer we will add to the HTML the "active" class if it's smoller than 10 it will remove "active" class if exist
-            if (window.Math.round(porcen) > 99) {
+            if (window.Math.round(porcen) > 95) {
                 // var activateButton = setTimeout(() => {
                 //     botonTop.classList.add("active")
                 // }, 200)
                 botonTop.classList.add("active")
-            } else if (window.Math.round(porcen) < 85) {
+            } else if (window.Math.round(porcen) < 90) {
                 botonTop.classList.remove("active")
             }
         })
     })
-})
+});
+
+const formInputName = document.querySelector('input[type="text"]')
+const formInputEmail = document.querySelector('input[type="email"]')
+const formInputCheckbox = document.querySelector('input[type="checkbox"]')
+
+// saves all the inputs into an array to clean the style 
+const arrStyles = [formInputName, formInputEmail, formInputCheckbox]
+
+const emailChecker = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+function validateForm() {
+    if (formInputName.value.length <= 2 || formInputName.value.length >= 100) {
+        formInputName.style.cssText = 'border-width: 0 0 2px 0;border-color: rgb(255, 0, 0);'
+    } else if (!emailChecker.test(formInputEmail.value)) {
+        formInputEmail.style.cssText = 'border-width: 0 0 2px 0;border-color: rgb(255, 0, 0);'
+    } else if (!formInputCheckbox.checked) {
+        formInputCheckbox.style.cssText = 'outline: 2px solid #ff0303;'
+    } else {
+        arrStyles.forEach(e => e.removeAttribute('style'))
+        document.querySelector("#submit-btn").innerHTML = "Sending...";
+        setTimeout(function () {
+            formInputName.value = ""
+            formInputEmail.value = ""
+            formInputCheckbox.checked = false
+            document.querySelector("#submit-btn").innerHTML = "Send";
+        }, 1000)
+    }
+}
